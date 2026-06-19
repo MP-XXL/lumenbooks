1e9398af65e7f5de8f0660b2cf6112710de94c01
MP-XXL <royceapera@yahoo.com>
2026-06-19 16:15:38 -0500

    Feat: Implements Featured Page

--- app/components/home/Featured.tsx
+++ app/components/home/Featured.tsx
@@ -1,11 +1,26 @@
+"use client"
 import React from 'react'
+import Image from "next/image"
+import narnia from "@/public/narnia.jpg"
 
 function Featured() {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div className="bg-[#1E1E1E] p-10">
+            <h2 className="font-bold text-3xl border-b-5 pb-1 w-fit border-b-white text-white">Featured</h2>
+            <div id="featured_grid" className="flex gap-20 overflow-x-auto my-10 p-5">
+                <div className="flex flex-col items-center">
+                    <Image className="w-28 rounded-md"
+                        alt="Narnia"
+                        src={narnia}
+                        placeholder="blur"
+                        quality={100}
+                    />
+                    <p className="text-white">Narnia</p>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default Featured
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Nav from "./components/shared/Nav";
 import BottomNav from "./components/shared/BottomNav";
 import Hero from "./components/home/Hero";
 import Explore from "./components/home/Explore";
+import Featured from "./components/home/Featured";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Nav />
       <Hero/>
       <Explore />
+      <Featured />
       <BottomNav />
     </div>
   );
