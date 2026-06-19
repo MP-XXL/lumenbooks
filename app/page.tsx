ed94703b0279b5b6bde0fa36e46b9b99c83270cc
MP-XXL <royceapera@yahoo.com>
2026-06-19 09:57:47 -0500

    Feat: Implements hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Nav from "./components/shared/Nav";
 import BottomNav from "./components/shared/BottomNav";
+import Hero from "./components/home/Hero";
 
 export default function Home() {
   return (
-    <div className="bg-[#0a0a0a]">
+    <div className="bg-[#0a0a0a] px-2.5">
       <Nav />
+      <Hero/>
       <BottomNav />
     </div>
   );
